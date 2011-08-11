46df0708898be07880bc1a7e78f7eb5b55b960d1
dwitya prawira <dwitya.prawira@yahoo.com>
2011-08-11 22:26:25 +0700

    new file

---
+++ php/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Login Form</title>
+<link rel="stylesheet" type="text/css" href="../css/main.css" />
+</head>
+
+<body>
+    <div id="mainPage">
+        <div id="loginForm">	
+        	<div id="imageLogin"></div>
+            <div id="loginField">
+            	<p>
+                	<label>Username</label>
+                    <input type="text" name="username" />                
+                </p>
+                <p>
+               		<label>Password</label>
+                    <input type="password" name="password" />
+                </p>
+            </div>
+        </div>
+    </div>
+    <div id="footer"></div>
+</body>
+</html>
