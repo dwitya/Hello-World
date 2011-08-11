40e7fdaffbd90db82818addc8da2108bc39a6672
dwitya prawira <dwitya.prawira@yahoo.com>
2011-08-11 22:40:43 +0700

    login

--- php/index.php
+++ php/index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<link rel="shortcut icon" href="../images/home.ico" />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Login Form</title>
 <link rel="stylesheet" type="text/css" href="../css/main.css" />
@@ -8,17 +9,21 @@
 
 <body>
     <div id="mainPage">
-        <div id="loginForm">	
-        	<div id="imageLogin"></div>
-            <div id="loginField">
-            	<p>
-                	<label>Username</label>
-                    <input type="text" name="username" />                
-                </p>
-                <p>
-               		<label>Password</label>
-                    <input type="password" name="password" />
-                </p>
+    	<div id="image"></div>
+        <div id="loginForm">
+        	<p>
+            	<label>Username</label>
+                <input type="text" id="textfield" name="username" />
+            </p>
+            <p>
+            	<label>Password</label>
+                <input type="password" id="textfield" name="password" />
+            </p>
+            <div class="buttons">
+                <a href="" class="regular"><!-- class="regular"-->
+                    <img src="../images/textfield_key.png" alt=""/>
+                    Login
+                </a>
             </div>
         </div>
     </div>
